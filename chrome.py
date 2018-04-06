541d5345fcb8d92c0f7921d7518cff58b8c4ed9f
Tianyu LIU <jeff_tianyu_liu@126.com>
2018-04-06 01:25:25 -0700

    alexa top 100 finished

--- chrome.py
+++ chrome.py
@@ -4,7 +4,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import sys, re, time
 
+#10 types - News, Sports, Shopping, Games, Computers, Adult, Science, Recreation, Health, Home
 Alexa_websites = []
+with open('alexa_top100.txt') as f:
+    Alexa_websites = f.readlines()
 
 chromedriver_path = "/Users/tianyuliu/Academics/CMU INI/2018 Spring/14828 - Browser Security/Project/Scripts/chromedriver"
 extension_path = "/Users/tianyuliu/Library/Application Support/Google/Chrome/Default/Extensions/cfhdojbkjhnklbpkdaibdccddilifddb/"
@@ -15,7 +18,8 @@ driver = webdriver.Chrome(executable_path=chromedriver_path, chrome_options = ch
 time.sleep(5)
 
 for website in Alexa_websites:
-    driver.get('https://www.' + site + '.com/')
-    driver.implicitly_wait(5)    
+    driver.get(website)
+    driver.implicitly_wait(5)  
+    time.sleep(3)  
 
 driver.quit()
