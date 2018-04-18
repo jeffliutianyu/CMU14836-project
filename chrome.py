411cb42649c4eb722b22ac6b6ba36fde0d56fb2d
Tianyu LIU <jeff_tianyu_liu@126.com>
2018-04-18 16:06:51 -0700

    chrome DNT scripts completed

--- chrome.py
+++ chrome.py
@@ -1,13 +1,19 @@
 
 import os
 from selenium import webdriver
+from browsermobproxy import Server
+import urlparse 
+import json
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 import sys, re, time
 import sqlite3
 
+##################################Function Calls######################################
 #retrieve cookie data from current page
-def retrieve_cookies():
+def retrieve_cookies_domains():
     #connect to sql database
+    file2 = open("chrome_adblockplus_cookie.txt","a")
     conn = sqlite3.connect('Results/Default/Cookies')
     db = conn.cursor()
 
@@ -18,41 +24,99 @@ def retrieve_cookies():
 
     for result in results:
         print("%s"%(result[0]))
+        file2.write(result[0]+" ")
+        file2.flush()
+    file2.write("\n")
+    file2.flush()
+    file2.close()
+
+#import test websites
+def import_testing_websites(number_websites_to_test, top1m_build, top1m_test):
+    count = 0
+    top1m = []
+    with open('top-1m.txt') as f:
+        top1m = f.readlines()
+    for website in top1m:
+        if count >= 4000:
+            break
+        if count < 2000:
+            top1m_build.append(website)
+            count = count + 1
+            continue
+        top1m_test.append(website)
+        count = count + 1
+        
 
 
-#10 types - News, Sports, Shopping, Games, Computers, Adult, Science, Recreation, Health, Home
-Alexa_websites = []
-with open('alexa_top100.txt') as f:
-    Alexa_websites = f.readlines()
-
+##############################Main Program############################################
 #path to git folder
 env_path = "/Users/tianyuliu/Academics/CMU INI/2018 Spring/14828 - Browser Security/Project/CMU14836-project/"
 env_path_2 = "/Users/tianyuliu/Academics/CMU\ INI/2018\ Spring/14828\ -\ Browser\ Security/Project/CMU14836-project/"
 #paths to chrome webdriver and extension
 chromedriver_path = env_path + "chromedriver"
-extension_path = "/Users/tianyuliu/Library/Application Support/Google/Chrome/Default/Extensions/cfhdojbkjhnklbpkdaibdccddilifddb/"
+proxy_path = env_path + "browsermob-proxy-2.1.4/bin/browsermob-proxy"
 
+#open a http proxy
+server = Server(proxy_path)
+server.start()
+proxy = server.create_proxy()
+parse_proxy = urlparse.urlparse(proxy.proxy).path
 
+#import testing websites 
+number_websites_to_test = 2000
+top1m_build = []
+top1m_test = []
+import_testing_websites(number_websites_to_test, top1m_build, top1m_test)
+
+#Edit options for chrome
 chop = Options()
 chop.add_extension('Adblock-Plus_v1.13.5.crx')
 chop.add_argument("--user-data-dir=Results")
+chop.add_argument('--proxy-server=%s' % parse_proxy)
 time.sleep(3)
 
-for website in Alexa_websites:
+#files to dump out tracking data
+file1 = open("chrome_DNT.txt","w")
+file2 = open("chrome_DNT_failedsites","w")
+#file2.close()
 
-    print(website)
+#04/18/ 00:04am - 0-999
+count = 0
+#collect data from all websites
+for website in top1m_test:
 
-    #browse the website and quit
-    driver = webdriver.Chrome(executable_path=chromedriver_path, chrome_options = chop)
+    print(website.strip())
 
-    driver.get(website)
-    driver.implicitly_wait(3)  
-    time.sleep(2)
-    driver.quit()
-    
-    #retrieve all the cookies
-    retrieve_cookies()
-    os.system("rm -rf " + env_path_2 + "/Results")
-    #break
+    try:
+        #browse the website and quit
+        driver = webdriver.Chrome(executable_path=chromedriver_path, chrome_options = chop)
+        url = "http://"+website.strip()
+        proxy.new_har(url, options={'captureHeaders': True})
+
+        driver.get(url)
+        time.sleep(3)
+        results = json.dumps(proxy.har)
+
+        file1.write(results)
+        file1.flush()
+        #retrieve all the cookies
+        #retrieve_cookies_domains()
+        driver.quit()
+        #os.system("rm -rf " + env_path_2 + "/Results")
+        time.sleep(0.5)
+        count = count + 1
+        #break
+
+    except:
+        file2.write(website+"\n")
+        file2.flush()
+        driver.quit()
+        time.sleep(0.5)
+        count = count + 1
+
+#stop server and close files
+server.stop()
+file1.close()
+file2.close()
 
 
